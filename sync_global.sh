e9311594148a2f5e4a8acd1cea419bda7353ba9f
Tj (bougyman) Vanderpoel <tj@rubyists.com>
2020-03-14 17:21:52 -0500

    Corrects symlink resolution

--- sync_global.sh
+++ sync_global.sh
@@ -5,13 +5,20 @@ usage() {
     echo PATH is the path to a confluence directory under the Global namespace
 }
 
+debug() {
+    [ -z "$debug" ] && return
+    local msg
+    msg=$*
+    echo "$msg" >&2
+}
+
 die() {
     local -i code
     local msg
     code=$1
     msg=$2
     echo "ERROR! : $msg" >&2
-    echo >&2 usage >&2
+    usage >&2
     # shellcheck disable=SC2086
     exit $code
 }
@@ -22,6 +29,10 @@ do
     if [ "$arg" = "--dry-run" ]
     then
         dry_run=true
+        continue
+    elif [ "$arg" = "--debug" ]
+    then
+        debug=true
         continue
     fi
     set -- "$@" "$arg"
@@ -43,22 +54,29 @@ local_path=Global/${confluence_path#*Global/}
 rsync --dry-run \
     -a --verbose --exclude="@versions/" --exclude="@exports/" \
     --include="*/" --include="*.txt" --exclude="*" \
-    "$confluence_path" "$local_path"  | while read -r line
+    "$confluence_path" "$local_path"  | tac | while read -r line
     do
         [[ "$line" =~ \.txt$ ]] || continue
 
+        debug "line: '$line'"
         filename=${line##*/}
-        conf_file=${confluence_path}/${filename}
+        debug "file: '$filename'"
+        conf_file=${confluence_path%/*}/${line}
+        debug "conf_file: '$conf_file'"
         local_dir=Global/${confluence_path#*Global/}
+        debug "local_dir: '$local_dir'"
         local_basedir=${local_dir%/*}
+        debug "local_basedir: '$local_basedir'"
         local_fullpath="$local_basedir/$line"
+        debug "local_fullpath: '$local_fullpath'"
         local_dirname=${local_fullpath%/*}
+        debug "local_dirname: '$local_dirname'"
         if [ -n "$dry_run" ]
         then
             [ -d "$local_dirname" ] || echo "'$local_dirname' (Directory) will be created"
-            echo "'$conf_file' will be linked to '$local_dirname/$filename'"
+            echo "'$conf_file' will be linked to '$local_fullpath'"
         else
             [ -d "$local_dirname" ] || mkdir -vp "$local_dirname"
-            ln -vs "$conf_file" "$local_dirname/$filename"
+            ln -vs "$conf_file" "$local_fullpath"
         fi
     done

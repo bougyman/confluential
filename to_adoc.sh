a0a3d9b38d8a61cf0d29669ccf817cf0373d1fb2
Tj (bougyman) Vanderpoel <tj@rubyists.com>
2020-04-05 19:35:42 -0500

    Adds support for confluence-asciidoc-converter

--- to_adoc.sh
+++ to_adoc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for dep in pandoc asciidoctor tidywiki.sh cpublish.sh
+for dep in pandoc tidywiki.sh cpublish.sh
 do
     if ! which "$dep" >/dev/null
     then
@@ -32,6 +32,7 @@ confxml=${confbare}.xml
 confraw=${confbare}-raw.adoc
 confmain=${confbare}.adoc
 confhtml=${confbare}.html
+confxhtml=${confbare}.xhtml
 
 # trap 'rm -f "$confxml" "$confraw"' EXIT
 
@@ -97,15 +98,31 @@ fi
 
 ${EDITOR:-vim} "$confmain"
 
-read -p "Would you like to convert '$confmain' to '$confhtml'? " -r yn
+read -p "Would you like to convert '$confmain' to '$confxhtml'? " -r yn
 if [[ "$yn" =~ ^[Yy][Ee]?$|^[Yy][Ee][Ss]$ ]]
 then
-    # Write the confluence-formatted output
-    asciidoctor -d article -se -b xhtml5 "$confmain"
+    # Write the confluence-formatted output using
+    # adoc_to_confluence.sh from https://github.com/amdrake93/asciidoc-confluence-converter
+    if which adoc_to_confluence.sh >/dev/null 2>&1
+    then
+        adoc_to_confluence.sh "$confmain"
+    # Fallback to asciidoctor if adoc_to_confluence.sh is not found
+    elif which asciidoctor >/dev/null 2>&1
+    then
+        echo "Could not find adoc_to_confluence.sh, using asciidoctor for conversion" >&2
+        echo "For full confluence syntax support, clone https://github.com/amdrake93/asciidoc-confluence-converter and symlink adoc_to_confluence.sh in your PATH" >&2
+        asciidoctor -d article -se -b xhtml5 "$confmain"
+        mv "$confhtml" "$confxhtml"
+    else
+        echo "ERROR: Could not find an asciidoc converter to use. 'adoc_to_confluence.sh' or 'asciidoctor' must be in your PATH" >&2
+        echo "For full confluence syntax support, clone https://github.com/amdrake93/asciidoc-confluence-converter and symlink adoc_to_confluence.sh in your PATH" >&2
+        echo "For basic confluence support, asciidoctor can be installed with 'gem install asciidoctor'" >&2
+        exit 2
+    fi
 
     read -p "$confhtml written. Would you like to publish it now? " -r yn
     if [[ "$yn" =~ ^[Yy][Ee]?$|^[Yy][Ee][Ss]$ ]]
     then
-        cpublish.sh "$confhtml"
+        cpublish.sh "$confxhtml"
     fi
 fi
